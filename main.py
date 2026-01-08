838b7171bfb73702826df851165965de9213414d
ivcoder <vank300828@gmail.com>
2026-01-08 15:18:11 +0000

    fix frame is not atrr err

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from sklearn.datasets import fetch_california_housing
 
 import pandas as pd
 
-data = fetch_california_housing
+data = fetch_california_housing(as_frame=True)
 df = data.frame
 
 print(df)
