4c4a772af199951fef85cdba8cabc36a47ce7635
ivcoder <vank300828@gmail.com>
2026-01-12 14:48:20 +0000

    add the function for printing sub header

--- lib.py
+++ lib.py
@@ -4,3 +4,17 @@ init(autoreset=True)
 
 def main_header(title):
     return Back.BLUE + "   === == = " + title + " = == ===   "
+
+def sub_header(title, length):
+    left_fill_block = ''
+    right_fill_block = ''
+
+    match length:
+        case 1:
+            left_fill_block = " === = "
+            right_fill_block = " = ==="
+        case 2:
+            left_fill_block = " === === = "
+            right_fill_block = " = === === "
+
+    return Back.GREEN + left_fill_block + title + right_fill_block
